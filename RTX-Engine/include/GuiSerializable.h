e405160a2b3437817371ee9cc58ec19a64246263
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2022-10-24 15:27:10 -0500

    Added serializable wrapper class

---
+++ RTX-Engine/include/GuiSerializable.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <string>
+
+template <class T>
+using GuiSerialized = std::vector<std::pair<std::string, T*>>;
+
+class GuiSerializable
+{
+public:
+	void SerializeFloat1(std::string name, GLfloat* float1);
+	void SerializeFloat2(std::string name, glm::vec2* float2);
+	void SerializeFloat3(std::string name, glm::vec3* float3);
+	
+	GuiSerialized<GLfloat>* GetFloat1s();
+	GuiSerialized<glm::vec2>* GetFloat2s();
+	GuiSerialized<glm::vec3>* GetFloat3s();
+
+private:
+	GuiSerialized<GLfloat> float1s;
+	GuiSerialized<glm::vec2> float2s;
+	GuiSerialized<glm::vec3> float3s;
+
+	void cpyVec2ToArr(glm::vec2* vec, GLfloat arr[2]);
+	void cpyVec3ToArr(glm::vec3* vec, GLfloat arr[3]);
+};
---
+++ RTX-Engine/src/GuiSerializable.cpp
@@ -0,0 +1,44 @@
+#include "GuiSerializable.h"
+
+void GuiSerializable::SerializeFloat1(std::string name, GLfloat* float1)
+{
+    float1s.push_back(std::make_pair(name, float1));
+}
+
+void GuiSerializable::SerializeFloat2(std::string name, glm::vec2* float2)
+{
+    float2s.push_back(std::make_pair(name, float2));
+}
+
+void GuiSerializable::SerializeFloat3(std::string name, glm::vec3* float3)
+{
+    float3s.push_back(std::make_pair(name, float3));
+}
+
+GuiSerialized<GLfloat>* GuiSerializable::GetFloat1s()
+{
+    return &float1s;
+}
+
+GuiSerialized<glm::vec2>* GuiSerializable::GetFloat2s()
+{
+    return &float2s;
+}
+
+GuiSerialized<glm::vec3>* GuiSerializable::GetFloat3s()
+{
+    return &float3s;
+}
+
+void GuiSerializable::cpyVec2ToArr(glm::vec2* vec, GLfloat arr[2])
+{
+    arr[0] = vec->x;
+    arr[1] = vec->y;
+}
+
+void GuiSerializable::cpyVec3ToArr(glm::vec3* vec, GLfloat arr[3])
+{
+    arr[0] = vec->x;
+    arr[1] = vec->y;
+    arr[2] = vec->z;
+}
